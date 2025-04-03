1b20b3e79836fb095fb0a63b7e6e185a87a66437
Афанасьева Милана Евгеньевна <152304@mck-ktits.ru>
2025-04-03 09:39:55 +0300

    Pr61

--- src/products.js
+++ src/products.js
@@ -3,18 +3,18 @@ export const products = [
         id: 0,
         name: 'Ароматизатор для автомобиля c ароматом "Кожа и древесина"',
         price: 654,
-        imagePath: `${import.meta.env.BASE_URL}src/img/Rectangle3.png`
+        imagePath: 'public/Rectangle3.png'
     },
     {
         id: 1,
         name: 'Ароматизатор для автомобиля Diamond candle',
         price: 345,
-        imagePath: `${import.meta.env.BASE_URL}src/img/Rectangle3.png`
+        imagePath:'public/Rectangle3.png'
     },
     {
         id: 2,
         name: 'Ароматизатор "Кожа и древесина"',
         price: 389,
-        imagePath: `${import.meta.env.BASE_URL}src/img/Rectangle3.png`
+        imagePath: 'public/Rectangle3.png'
     },
 ]
